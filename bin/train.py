7588565599358a40e61ba0da8f7636b48d587dfa
sophiepavia <spavia@me.com>
2024-12-11 19:54:52 -0600

    example notebook run

--- bin/train.py
+++ bin/train.py
@@ -74,37 +74,6 @@ def plot_sample_efficiency(log_dir):
     plt.savefig(os.path.join(log_dir, "sample_efficiency.png"))
     # plt.show()
 
-def evaluate(model, num_episodes=10, deterministic=True):
-    """
-    Evaluate a RL agent
-    :param model: (BaseRLModel object) the RL Agent
-    :param num_episodes: (int) number of episodes to evaluate it
-    :return: (float) Mean reward for the last num_episodes
-    """
-    # This function will only work for a single Environment
-    vec_env = model.get_env()
-    all_episode_rewards = []
-    for i in range(num_episodes):
-        episode_rewards = []
-        done = False
-        obs = vec_env.reset()
-        while not done:
-            # _states are only useful when using LSTM policies
-            action, _states = model.predict(obs, deterministic=deterministic)
-            # here, action, rewards and dones are arrays
-            # because we are using vectorized env
-            # also note that the step only returns a 4-tuple, as the env that is returned
-            # by model.get_env() is an sb3 vecenv that wraps the >v0.26 API
-            obs, reward, done, info = vec_env.step(action)
-            episode_rewards.append(reward)
-
-        all_episode_rewards.append(sum(episode_rewards))
-
-    mean_episode_reward = np.mean(all_episode_rewards)
-    print("Mean reward:", mean_episode_reward, "Num episodes:", num_episodes)
-
-    return mean_episode_reward  
-
 def train(method, env_name, hyperparams, tensor_board_dir, log_dir):
     # For make_vec_env
     n_envs = hyperparams.pop("n_envs", 1)
